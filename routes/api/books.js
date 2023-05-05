b95daab01f01dfa894bfef8577ccdb37355b5030
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-05-05 17:14:17 +0300

    ...

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const Joi = require("joi");
 const books = require("../../models/books");
 const { HttpError } = require("../../helpers");
 
 const router = express.Router();
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+});
 
 router.get("/", async (req, res) => {
   try {
@@ -21,6 +27,49 @@ router.get("/:id", async (req, res, next) => {
       throw HttpError(404, "Not found");
     }
     res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await books.add(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await books.updateById(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await books.deleteById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
