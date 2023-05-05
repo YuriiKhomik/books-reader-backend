b14acd563d4a874b2b9bdab3b7ef12b68dbb1cfb
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-05-05 10:50:46 +0300

    ...

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,12 +1,29 @@
 const express = require("express");
-
 const books = require("../../models/books");
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const result = await books.getAll();
-  res.json(result);
+  try {
+    const result = await books.getAll();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await books.getById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
