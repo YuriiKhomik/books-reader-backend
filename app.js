8150f9fa789123382bebb58a7ca74571827c5479
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-05-15 20:26:44 +0300

    ...

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const booksRouter = require("./routes/api/books");
+const authRouter = require("./routes/api/auth");
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/books", booksRouter);
 
 app.use((req, res) => {
---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth");
+
+const { validateBody } = require("../../middlewares");
+
+const { schemas } = require("../../models/user");
+
+const router = express.Router();
+
+// signup
+router.post("/register", ctrl.register);
+
+module.exports = router;
